faf9bebaf5198c4af71811f7da868862cf438e1e
Dickens-odera <dickensodera9@gmail.com>
2019-03-05 03:30:10 +0300

    Create a posts model

--- psql/posts/admin.py
+++ psql/posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Posts
 # Register your models here.
+
+class RegisterPostsToAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Posts
+
+admin.site.register(Posts,RegisterPostsToAdmin)
--- psql/posts/models.py
+++ psql/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Posts(models.Model):
+    title = models.CharField(max_length = 100)
+    description = models.CharField(max_length = 100)
+    author = models.CharField(max_length = 50)
+
+
+    def __str__(self):
+        return self.title
