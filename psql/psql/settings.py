7e9ae0e0c7e6d98edb171fac3d4309483b79b4dd
Dickens-odera <dickensodera9@gmail.com>
2019-03-05 03:22:52 +0300

    configuring postgresql database

--- psql/psql/settings.py
+++ psql/psql/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settrings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'psql.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'project1',
+        'USER':'postgres',
+        'PASSWORD':12211009731974,
+        'HOST':'localhost',
+        'PORT':5432
     }
 }
 
