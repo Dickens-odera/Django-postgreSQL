e06e32fdb5262daea5b4a71b6f94fa8ec34ac1ea
Dickens-odera <dickensodera9@gmail.com>
2019-03-07 19:32:21 +0300

    class based views in django

--- psql/students/views.py
+++ psql/students/views.py
@@ -3,9 +3,4 @@ from django.views import View
 
 class Student(View):
     # default constructor
-    def __init__(self):
-        pass
     
-    def Register(self, *args, **kwargs):
-        self.name
-
